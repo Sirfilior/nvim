6a87a322797f0b44389324643d245a8858506eff
dominika <dominik.albrecht@intersim.ch>
2023-07-06 22:46:28 +0200

    Expand cmp

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -1,16 +1,32 @@
-return { {
-	-- Autocompletion
-	'hrsh7th/nvim-cmp',
-	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
-		'L3MON4D3/LuaSnip',
-		'saadparwaiz1/cmp_luasnip',
+return {
+	{
+		-- Autocompletion
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
 
-		-- Adds LSP completion capabilities
-		'hrsh7th/cmp-nvim-lsp',
+			-- Adds LSP completion capabilities
+			"hrsh7th/cmp-nvim-lsp",
+			"onsails/lspkind-nvim",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
 
-		-- Adds a number of user-friendly snippets
-		'rafamadriz/friendly-snippets',
+			-- Adds a number of user-friendly snippets
+			"rafamadriz/friendly-snippets",
+		},
 	},
-},
+	{
+		"zbirenbaum/copilot.lua",
+		config = function()
+			require("copilot").setup()
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
