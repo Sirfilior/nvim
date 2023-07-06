b99ab66b4a63c43074eeafc4a644517402e2a2cf
dominika <dominik.albrecht@intersim.ch>
2023-07-06 22:48:09 +0200

    Switch to compilot zbirenbaum

--- lua/custom/plugins/codetools.lua
+++ lua/custom/plugins/codetools.lua
@@ -1,22 +1,22 @@
 return {
 	-- Detect tabstop and shiftwidth automatically
-	{ 'tpope/vim-sleuth' },
-	{ 'tpope/vim-abolish' },
-	{ 'mbbill/undotree' },
-	{ 'github/copilot.vim' },
-	{ 'kylechui/nvim-surround' },
-	{ 'gbprod/substitute.nvim' },
+	{ "tpope/vim-sleuth" },
+	{ "tpope/vim-abolish" },
+	{ "mbbill/undotree" },
+	-- { 'github/copilot.vim' },
+	{ "kylechui/nvim-surround" },
+	{ "gbprod/substitute.nvim" },
 	{
 		-- Add indentation guides even on blank lines
-		'lukas-reineke/indent-blankline.nvim',
+		"lukas-reineke/indent-blankline.nvim",
 		-- Enable `lukas-reineke/indent-blankline.nvim`
 		-- See `:help indent_blankline.txt`
 		opts = {
-			char = '┊',
+			char = "┊",
 			show_trailing_blankline_indent = false,
 		},
 	},
 
 	-- "gc" to comment visual regions/lines
-	{ 'numToStr/Comment.nvim', opts = {} },
+	{ "numToStr/Comment.nvim", opts = {} },
 }
