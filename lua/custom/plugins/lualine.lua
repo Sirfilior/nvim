a4a1482bd3de566daeae4159f22c210f14ebc1a8
dominika <dominik.albrecht@intersim.ch>
2023-07-29 14:53:18 +0200

    Relative path lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,17 +1,29 @@
 return {
-    {
-        -- Set lualine as statusline
-        "nvim-lualine/lualine.nvim",
-        -- See `:help lualine.txt`
-        dependencies = {
-            { "nvim-tree/nvim-web-devicons", opt = true },
-        },
-        opts = {
-            options = {
-                theme = "catppuccin",
-                component_separators = "|",
-                section_separators = "",
-            },
-        },
-    },
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		-- See `:help lualine.txt`
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons", opt = true },
+		},
+		opts = {
+			options = {
+				theme = "catppuccin",
+				component_separators = "|",
+				section_separators = "",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- 0: Just the filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
+					},
+				},
+			},
+		},
+	},
 }
