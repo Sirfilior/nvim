cc3b9e5ae7a58c72b992db083e43932f0970d850
dominika <dominik.albrecht@intersim.ch>
2023-09-14 19:34:38 +0200

    Setup ts lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,7 @@ end
 local null_ls = require("null-ls")
 local icons = require("config.icons")
 local autoformat = require("config.autoformat")
+local lsp_custom = require("custom.lsp")
 
 null_ls.setup({
 	sources = {
@@ -14,6 +15,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.djlint,
 		null_ls.builtins.diagnostics.ruff,
+		require("typescript.extensions.null-ls.code-actions"),
 	},
 	on_attach = function(client, bufnr)
 		autoformat.lsp_format_attach(client, bufnr)
@@ -21,57 +23,12 @@ null_ls.setup({
 })
 
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(client, bufnr)
-	if client.server_capabilities.documentSymbolProvider then
-		require("nvim-navic").attach(client, bufnr)
-	end
-
-	-- In this case, we create a function that lets us more easily define mappings specific
-	-- for LSP related items. It sets the mode, buffer and description for us each time.
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = "LSP: " .. desc
-		end
-
-		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-	end
-
-	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-	-- See `:help K` for why this keymap
-	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-	-- Lesser used LSP functionality
-	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-	nmap("<leader>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "[W]orkspace [L]ist Folders")
-
-	autoformat.lsp_format_attach(client, bufnr)
-
-	-- Create a command `:Format` local to the LSP buffer
-	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-		vim.lsp.buf.format()
-	end, { desc = "Format current buffer with LSP" })
-end
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-	tsserver = {},
 	rust_analyzer = {},
 	svelte = {},
 	html = {},
@@ -120,7 +77,7 @@ local attachOverrides = {
 		-- Disable hover in favor of Pyright
 		client.server_capabilities.hoverProvider = false
 
-		on_attach(client, bufnr)
+		lsp_custom.on_attach(client, bufnr)
 	end,
 }
 
@@ -142,7 +99,7 @@ mason_lspconfig.setup_handlers({
 	function(server_name)
 		require("lspconfig")[server_name].setup({
 			capabilities = capabilities,
-			on_attach = attachOverrides[server_name] or on_attach,
+			on_attach = attachOverrides[server_name] or lsp_custom.on_attach,
 			settings = servers[server_name],
 		})
 	end,
