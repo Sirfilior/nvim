60dd3d8d61e9497f2461717f95f6d57878459ee7
dominika <dominik.albrecht@intersim.ch>
2023-09-22 10:44:38 +0200

    Larger harpoon

--- lua/main/plugins/harpoon.lua
+++ lua/main/plugins/harpoon.lua
@@ -1,6 +1,11 @@
 return {
   {
     "theprimeagen/harpoon",
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 20,
+      },
+    },
     keys = {
       {
         "<leader>a",
