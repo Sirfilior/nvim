25529b67612af4a3cd492cfe6d21a8da0d124367
dominika <dominik.albrecht@intersim.ch>
2023-11-17 15:48:56 +0100

    Install typoscript parser

--- lua/main/plugins/lang/php.lua
+++ lua/main/plugins/lang/php.lua
@@ -5,7 +5,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "php", "twig" })
+        vim.list_extend(opts.ensure_installed, { "php", "twig", "typoscript" })
       end
     end,
   },
