9976629ffe142af6d913ee09566bdd20a20ffc52
dominika <dominik.albrecht@intersim.ch>
2024-06-14 09:08:48 +0200

    fix vue ts plugin

--- lua/main/plugins/lang/vue.lua
+++ lua/main/plugins/lang/vue.lua
@@ -18,7 +18,25 @@ return {
     opts = {
       servers = {
         volar = {},
-        vtsls = {},
+        vtsls = {
+          before_init = function(params, config)
+            local result = vim.system(
+              { "npm", "query", "#vue" },
+              { cwd = params.workspaceFolders[1].name, text = true }
+            ):wait()
+            if result.stdout ~= "[]" then
+              local vuePluginConfig = {
+                name = "@vue/typescript-plugin",
+                location = require("mason-registry").get_package("vue-language-server"):get_install_path()
+                    .. "/node_modules/@vue/language-server",
+                languages = { "vue" },
+                configNamespace = "typescript",
+                enableForWorkspaceTypeScriptVersions = true,
+              }
+              table.insert(config.settings.vtsls.tsserver.globalPlugins, vuePluginConfig)
+            end
+          end,
+        },
       },
     },
   },
@@ -28,15 +46,6 @@ return {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       table.insert(opts.servers.vtsls.filetypes, "vue")
-      Util.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
-        {
-          name = "@vue/typescript-plugin",
-          location = Util.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-          languages = { "vue" },
-          configNamespace = "typescript",
-          enableForWorkspaceTypeScriptVersions = true,
-        },
-      })
     end,
   },
 }
